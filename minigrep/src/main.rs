82432fc1b03dc8239fadb21a2234329926a28f72
Ray Lin <lieeray4136@gmail.com>
2017-12-24 18:41:07 +0800

    done minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,42 +1,23 @@
+extern crate minigrep;
+
 use std::env;
 use std::process;
-use std::fs::File;
-use std::io::prelude::*;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err); 
+        eprintln!("Problem parsing arguments: {}", err); 
         process::exit(1);
     });
 
-    println!("{:?}", args);
-    println!("query: {}, filename: {}", config.query, config.filename);
+    // println!("{:?}", args);
+    // println!("query: {}, filename: {}", config.query, config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found.");
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents)
-        .expect("something went wrong when read file.");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!"); 
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-   
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e); 
+        process::exit(1);
     }
 }
